394f8d4cb9da6e121ba7a8db6a06ba3b181642b7
wolfdog940 <oscarurrutia940@gmail.com>
2023-01-13 17:43:34 +0100

    press enter

--- todo.js
+++ todo.js
@@ -3,41 +3,46 @@ let input =document.getElementById("todoInput");
 let button =document.getElementById("button")
 let ul=document.getElementById("ul")
 
-button.addEventListener("click",function(){
+input.addEventListener("keypress",function(event){
 
-    let str =input.value.trim()
-    let add=str.charAt(0).toUpperCase() + str.slice(1)
-    let newbutton =document.createElement("button")
-    
-    
+    if (event.key === "Enter") {  
 
-    if (add.length){
-    
-    let lista=document.createElement("li");
-    lista.innerHTML=add
-    ul.appendChild(lista)
-    lista.appendChild(newbutton)
-    input.value=""
-    
-    newbutton.addEventListener("click",function(){
-        console.log(lista)
-        ul.removeChild(lista)
-    
-     })
-    lista.addEventListener("click",function(){
-        console.log(lista)
-        lista.style.color="black"
-        lista.style.backgroundColor="rgba(207, 125, 31, 0.2)"
-        lista.style.b
+        event.preventDefault();
+        let str =input.value.trim()
+        let add=str.charAt(0).toUpperCase() + str.slice(1)
+        let newbutton =document.createElement("button")
         
-        lista.innerHTML=add.strike()
-        lista.appendChild(newbutton)
+        
     
-     })
-}
-    else{
-        alert("el input esta vacio")
+        if (add.length){
+        
+        let lista=document.createElement("li");
+        lista.innerHTML=add
+        ul.appendChild(lista)
+        lista.appendChild(newbutton)
+        input.value=""
+        
+        newbutton.addEventListener("click",function(){
+            console.log(lista)
+            ul.removeChild(lista)
+        
+         })
+        lista.addEventListener("click",function(){
+            console.log(lista)
+            lista.style.color="black"
+            lista.style.backgroundColor="rgba(207, 125, 31, 0.2)"
+            lista.style.b
+            
+            lista.innerHTML=add.strike()
+            lista.appendChild(newbutton)
+        
+         })
     }
+        else{
+            alert("el input esta vacio")
+        }}
+
+  
     
    
    
